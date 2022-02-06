41fb60db832aae4458daa2b9b46e8ac1ec905fa3
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-06 11:28:29 +0900

    adding space for num2

--- Exercises-1-Basic/src/exercises/Num2.java
+++ Exercises-1-Basic/src/exercises/Num2.java
@@ -1,18 +1,18 @@
 package exercises;
+
 /*
  * Write a program to calculate the sum of first 10 natural number.
  */
 public class Num2 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int i=1;
-		int sum = 0; 
-		for (i=1;i<=10;i++) {
-			sum = sum + i;
-		}
-		
-		System.out.println("Sum: " + sum);
+    public static void main(String[] args) {
+	int i = 1;
+	int sum = 0;
+	for (i = 1; i <= 10; i++) {
+	    sum = sum + i;
 	}
+
+	System.out.println("Sum: " + sum);
+    }
 
 }
