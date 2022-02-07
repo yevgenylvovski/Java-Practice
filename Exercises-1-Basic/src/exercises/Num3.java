e0ebf2cfae235595d1823e98c55bf63b8812f14f
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-07 14:12:52 +0900

    prompted user

--- Exercises-1-Basic/src/exercises/Num3.java
+++ Exercises-1-Basic/src/exercises/Num3.java
@@ -1,18 +1,29 @@
 package exercises;
 
+import java.util.Scanner;
+
 /*
  * Write a program that prompts the user to input a positive integer.
  *  It should then print the multiplication table of that number. 
  */
+
 public class Num3 {
 
     public static void main(String[] args) {
-	int i = 1;
-	int result = 0;
-	for (i = 1; i <= 10; i++) {
-	    result = i * 4;
-	    System.out.printf("%d*%d=%d", 5, i, result).println();
-	}
-    }
 
+	Scanner sc = new Scanner(System.in);
+
+	System.out.println("Your number: ");
+	int Yournumber = sc.nextInt();
+
+	int result = 0;
+	int i = 1;
+	for (i = 1; i <= 10; i++) {
+	    result = i * Yournumber;
+	    System.out.printf("%d*%d=%d", Yournumber, i, result).println();
+
+	}
+	sc.close();
+
+    }
 }
