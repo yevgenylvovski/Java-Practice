8f1bb481df5665bf66a6cd3ed59a150f0d7682c4
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-07 10:36:22 +0900

    prompted the user

--- Exercises-2-Basic/src/exercises/Num1.java
+++ Exercises-2-Basic/src/exercises/Num1.java
@@ -1,24 +1,33 @@
 package exercises;
 
+import java.util.Scanner;
+
 /* 1.
 Write a Java program to print the sum (addition), multiply, subtract, divide and 
 remainder of two numbers.
 Test Data:
-Input first number: 125
-Input second number: 24
+Input firstnumber: 125
+Input secondnumber: 24
 Expected Output :
 125 + 24 = 149
 125 - 24 = 101
 125 x 24 = 3000
 125 / 24 = 5
-125 mod 24 = 5*/
+125 % 24 = 5*/
 
 public class Num1 {
 
     public static void main(String[] args) {
-
-	int firstnumber = 200;
-	int secondnumber = 50;
+	
+	Scanner in = new Scanner(System.in);
+	   
+	  System.out.print("Input first number: ");
+	  int firstnumber = in.nextInt();
+	   
+	  System.out.print("Input second number: ");
+	  int secondnumber = in.nextInt();
+// Important note: The second number must be bigger than 0
+	
 	int result;
 
 	// +
