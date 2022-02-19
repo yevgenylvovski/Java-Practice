03c700d7582d4df9fa03577ad92dec8bcd4182a9
yevgenyLvov <yevgeny_4@hotmail.com>
2022-02-19 10:15:01 +0900

    fix

--- Exercises-1-Basic/src/exercises/Num2.java
+++ Exercises-1-Basic/src/exercises/Num2.java
@@ -1,15 +1,7 @@
 package exercises;
-<<<<<<< HEAD
+
 // Write a program to calculate the sum of first 10 natural number.
-// 1+2+3+4+5+6+7+8+9+10 
-=======
-
-/*
- * Write a program to calculate the sum of first 10 natural number.
-1+2+3+4+5+6+7+8+9+10
-
- */
->>>>>>> branch 'exercise1-alisa' of https://github.com/yevgenylvovski/Java-Practice.git
+// 1+2+3+4+5+6+7+8+9+10
 public class Num2 {
     public static void main(String[] args) {
 	int i = 1;
