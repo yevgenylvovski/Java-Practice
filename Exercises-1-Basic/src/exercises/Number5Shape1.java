1a73554f5356d2677cd9d63a05d163e16981edd8
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-23 18:33:11 +0900

    update number5 excercise

---
+++ Exercises-1-Basic/src/exercises/Number5Shape1.java
@@ -0,0 +1,17 @@
+package exercises;
+
+/*
+ *  i) do shape
+**********
+**********
+**********
+**********
+ */
+public class Number5Shape1 {
+    public static void main(String[] args) {
+	int i = 1;
+	for (i = 1; i <= 5; i++) {
+	    System.out.println("**********");
+	}
+    }
+}
