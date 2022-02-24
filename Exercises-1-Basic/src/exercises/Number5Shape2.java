5fc9e70e0fe8ea93c3a3d8e04dc708289280ced7
Tran Kim Hoang <98681959+Alisakimhoang@users.noreply.github.com>
2022-02-24 19:06:12 +0900

    Update Exercises-1-Basic/src/exercises/Number5Shape2.java
    Co-authored-by: yevgenylvovski <46200614+yevgenylvovski@users.noreply.github.com>

--- Exercises-1-Basic/src/exercises/Number5Shape2.java
+++ Exercises-1-Basic/src/exercises/Number5Shape2.java
@@ -10,8 +10,9 @@ public class Number5Shape2 {
     public static void main(String[] args) {
 	int number = 1;
 	while (number <= 5) {
-	    for (int i = 1; i <= number; i++)
+	    for (int i = 1; i <= number; i++) {
 		System.out.print("*");
+	    }
 	    number++;
 	    System.out.println();
 	}
