c76180ce55132e7695f2437533e734d5f196899e
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-24 18:56:22 +0900

    Removed dead line for Num3, exercise 1

--- Exercises-1-Basic/src/exercises/Num3.java
+++ Exercises-1-Basic/src/exercises/Num3.java
@@ -3,7 +3,6 @@ package exercises;
 import java.util.Scanner;
 // Write a program that prompts the user to input a positive integer.
 // It should then print the multiplication table of that number.  
-
 public class Num3 {
     public static void main(String[] args) {
 	Scanner sc = new Scanner(System.in);
