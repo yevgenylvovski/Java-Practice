f987f915c791aa66e29c265e81db088a397e18db
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-24 18:44:41 +0900

    update exercise1, num 2

--- Exercises-1-Basic/src/exercises/Num2.java
+++ Exercises-1-Basic/src/exercises/Num2.java
@@ -4,12 +4,10 @@ package exercises;
 // 1+2+3+4+5+6+7+8+9+10
 public class Num2 {
     public static void main(String[] args) {
-	int i = 1;
 	int sum = 0;
-	for (i = 1; i <= 10; i++) {
+	for (int i = 1; i <= 10; i++) {
 	    sum = sum + i;
 	}
 	System.out.println("Sum: " + sum);
     }
-
 }
