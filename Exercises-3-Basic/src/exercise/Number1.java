51292f2226318642123f4077c66b8671c1238d3f
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-02-28 17:59:17 +0900

    update exercise num1

--- Exercises-3-Basic/src/exercise/Number1.java
+++ Exercises-3-Basic/src/exercise/Number1.java
@@ -7,7 +7,12 @@ package exercise;
 public class Number1 {
 
     public static void main(String[] args) {
+	int []arr = new int[] {2,5,1,4,10};
+	int sum = 0;
+	for (int i = 0; i < arr.length; i++) {
+	    sum = sum + arr[i];
+	}
+	    System.out.println("Sum of all the value: "+sum);
+	}
 
     }
-
-}
