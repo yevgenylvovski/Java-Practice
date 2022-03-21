9649bccebfa50858682b2e09d6400947c69659ee
Tran Kim Hoang <Alisakimhoang@gmail.com>
2022-03-21 13:29:15 +0900

    formatting

--- Exercises-3-Basic/src/exercise/Number2.java
+++ Exercises-3-Basic/src/exercise/Number2.java
@@ -6,16 +6,16 @@ package exercise;
 //Output: should be the average value: (2 + 5 + 1 + 4 + 10) / 5 = 4.4.
 public class Number2 {
 
-    public static void main(String[] args) {
-        int[] arr = new int[] { 2, 5, 1, 4, 10 };
+	public static void main(String[] args) {
+		int[] arr = new int[] { 2, 5, 1, 4, 10 };
 
-        double sum = 0;
+		double sum = 0;
 
-        for (int i = 0; i < arr.length; i++) {
-            sum = sum + arr[i];
-        }
+		for (int i = 0; i < arr.length; i++) {
+			sum = sum + arr[i];
+		}
 
-        double average = sum / 5;
-        System.out.println("Average value: " + average);
-    }
+		double average = sum / 5;
+		System.out.println("Average value: " + average);
+	}
 }
